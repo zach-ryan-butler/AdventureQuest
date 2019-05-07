896157f29eff8511fbe33bad453b1ef29711f658
Zach Butler <zach.standley93@gmail.com>
2019-05-06 19:41:36 -0700

    made makeUser function and imported to its own folder

---
+++ src/home/make-user.js
@@ -0,0 +1,11 @@
+function makeUser() {
+    const user = {
+        name: 'ash',
+        pokemon: 'pikachu',
+        hp: 50,
+        exp: 0
+    };
+    return user;
+}
+
+export default makeUser;
