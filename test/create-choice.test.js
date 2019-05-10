6a7b569b50b1af78d1fd780a219c66df1dfc1ca2
Zach Butler <zach.standley93@gmail.com>
2019-05-09 17:33:54 -0700

    TDD create-choice.test

--- test/create-choice.test.js
+++ test/create-choice.test.js
@@ -1,19 +1,41 @@
 const test = QUnit.test;
 
 function createChoice(choice) {
+    const div = document.createElement('div');
+    const label = document.createElement('label');
+    const input = document.createElement('input');
+
+    label.setAttribute('for', choice.id);
+    label.textContent = 'Battle them';
     
+    input.setAttribute('id', choice.id);
+    input.setAttribute('type', 'radio');
+    input.setAttribute('name', 'choice-buttons');
+    input.setAttribute('value', choice.id);
+    
+    div.appendChild(label);
+    div.appendChild(input);
+
+    return div;
 }
 
 test('make choice radio buttons from choice object', function(assert) {
     //arrange
     const choice = {
         id: 'fight',
-        description: 'Battle them'
+        description: 'Battle them',
+        result: `
+            Steeling your resolve you step forward and
+            challenge the gym leader. After a fierce struggle the
+            outcome is obvious. You win, but lose 20hp and gain 30 exp
+        `,
+        hp: -20,
+        exp: 30
     };
 
     const expected = '<div><label for="fight">Battle them</label><input id="fight" type="radio" name="choice-buttons" value="fight"></div>';
     //act
-    const result = createChoice(choice);
+    const dom = createChoice(choice);
     //assert
-    assert.deepEqual(result, expected);
+    assert.deepEqual(dom.outerHTML, expected);
 });
--- test/tests.js
+++ test/tests.js
@@ -2,3 +2,4 @@ import './make-user.test.js';
 import './api.test.js';
 import './create-quest-link.test.js';
 import './create-completed-quests.test.js';
+import './create-choice.test.js';
